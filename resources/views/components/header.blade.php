4571850ec4bd789f26a469c4ca35ae2a47973190
rarap <11734421+rarap@users.noreply.github.com>
2025-01-21 17:09:17 +0100

    First attempt for transfer page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<header class="bg-orange-900 text-white p-4 mb-5 border-b-4 border-orange-600">
+
+    <div class="max-w-screen-lg grid mx-auto grid-cols-2 p-2 pl-4">
+        <div>
+        <p class="font-semibold">Dhamma-Reise</p>
+         Wir hoffen, dass Ihnen die Seite eine angenehme Anreise und nette Bekanntschaften ermöglicht.
+        <br/><br/>
+        <p class="font-semibold">Bitte beachten Sie:</p>
+        Geben Sie Ihre Mailadresse ein, diese ist nicht öffentlich sichtbar. Geben Sie aber zum Schutz Ihrer Privatsphäre <u>NIEMALS</u> Daten wie Ihren Nachnamen, Ihre Telefonnummer, Adresse oder andere persönliche Daten an, denn diese Daten können von allen Besuchern der Seite eingesehen werden.
+
+        </div>
+        <div class="p-2 pl-4">
+            <img src="/images/stille.jpg" alt="Mönche und Nonne in tiefer Andacht" title="Mönche und Nonne in tiefer Andacht">
+        </div>
+    </div>
+
+    </nav>
+</header>
---
+++ resources/views/components/transfer.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+<div>
+   Transfer
+</div>
+</x-layout>
